3f396225f7e36a941a0b67e8338fe8ea73b7375c
monthem <lpoeh01@gmail.com>
2020-09-07 16:47:17 +0900

    MODIFY: added delete() method to WaitingLine.ts

--- src/websocket/routes/Model/WaitingLine.ts
+++ src/websocket/routes/Model/WaitingLine.ts
@@ -11,11 +11,25 @@ class WaitingLine {
     return this;
   }
 
+  delete(player: Player | number) {
+    this.line = this.line.filter((entry) => {
+      if (typeof player === 'number') {
+        entry.id !== player;
+      } else {
+        entry !== player
+      }
+    });
+    return this;
+  }
+
   findMatch(player: Player) {
     return new Promise<[Player, Player]>((resolve, reject) => {
-      const opponent = this.line.pop();
-      if (opponent) {
+      const opponents = this.line.filter((entry) => entry !== player);
+      const opponent = opponents.pop();
+      if (opponent && opponent !== player) {
         resolve ([opponent, player]);
+        this.line = this.line
+          .filter((entry) => entry !== player && entry !== opponent)
       } else {
         reject(null);
       }
