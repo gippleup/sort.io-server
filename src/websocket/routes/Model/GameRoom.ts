55289e00ac8cf71d5951437454efe825f8ec6cb7
monthem <lpoeh01@gmail.com>
2020-09-14 20:26:23 +0900

    ADD: added methods to GameRoom: decideDifficulty, resetTimers, askRematch, cancelRematchAsk, alertRematchDeclined

--- src/websocket/routes/Model/GameRoom.ts
+++ src/websocket/routes/Model/GameRoom.ts
@@ -5,6 +5,7 @@ import * as socketAction from '../../action/creator';
 import { MapOption } from '../../../algo/generateMap';
 import { getRepository } from 'typeorm';
 import { MultiPlay } from '../../../entity/MultiPlay';
+import { getMultiPlayRankByUserId } from '../../../utils/multiPlay';
 
 export type MapDesc = MapOption & { difficulty: number };
 
@@ -13,23 +14,22 @@ class GameRoom {
   players: Player[] = [];
   winner: number = -1;
   createdAt: string | undefined;
-  map: number[][];
-  mapDesc: MapDesc;
+  map: number[][] | undefined;
+  mapDesc: MapDesc | undefined;
   gameDuration: number = 120;
   interval: NodeJS.Timer | undefined;
   leftTime: number = this.gameDuration;
   leftPrepareTime: number = 3;
   prepareTimer: NodeJS.Timer | undefined;
   fps: number = 1;
-  constructor(player1: Player, player2: Player) {
+  constructor (player1: Player, player2: Player) {
     this.players.push(player1, player2);
     this.createdAt = new Date(Date.now()).toUTCString();
-    const {question, desc} = generateMultiMap(1); // TODO: set difficulty by comparing player1,2 level;
-    this.map = question;
-    this.mapDesc = desc;
     this.roomId = GameRoom.count;
     GameRoom.count += 1;
 
+    this.generateMap = this.generateMap.bind(this);
+    this.decideDiffiulty = this.decideDiffiulty.bind(this);
     this.forEachClient = this.forEachClient.bind(this);
     this.forEachPlayer = this.forEachPlayer.bind(this);
     this.sendRoomData = this.sendRoomData.bind(this);
@@ -46,13 +46,56 @@ class GameRoom {
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
     this.restartTimer = this.restartTimer.bind(this);
+    this.resetTimers = this.resetTimers.bind(this);
     this.updateScore = this.updateScore.bind(this);
     this.checkWinner = this.checkWinner.bind(this);
     this.informWinner = this.informWinner.bind(this);
+    this.askRematch = this.askRematch.bind(this);
+    this.cancelRematchAsk = this.cancelRematchAsk.bind(this);
+    this.alertRematchDeclined = this.alertRematchDeclined.bind(this);
     this.alertDockAction = this.alertDockAction.bind(this);
     this.saveResult = this.saveResult.bind(this);
     this.endGame = this.endGame.bind(this);
     this.terminateConnections = this.terminateConnections.bind(this);
+  }
+
+  async decideDiffiulty() {
+    const tasks = this.players.map((player) => {
+      return getMultiPlayRankByUserId(player.id)
+      .then((data) => {
+        if (data?.targetUser) {
+          return data.targetUser.KBI
+        } else {
+          return 0
+        }
+      })
+    });
+    const KBI = await Promise.all(tasks);
+    const minKBI = KBI.reduce((acc, ele) => {
+      if (acc > ele) return ele;
+      return acc;
+    }, 0)
+
+    if (minKBI < 90) {
+      return 1;
+    } else if (minKBI < 150) {
+      return 2;
+    } else if (minKBI < 210) {
+      return 3;
+    } else if (minKBI < 240) {
+      return 4;
+    } else {
+      return 5;
+    }
+  }
+
+  generateMap() {
+    return this.decideDiffiulty()
+      .then((difficulty) => {
+        const { question, desc } = generateMultiMap(difficulty);
+        this.map = question;
+        this.mapDesc = desc;
+      })
   }
 
   forEachPlayer(callback: (player: Player) => any) {
@@ -69,12 +112,17 @@ class GameRoom {
 
   sendRoomData() {
     const {map, mapDesc, roomId} = this;
+    const playerData = this.players.map((player) => ({
+      name: player.name,
+      id: player.id,
+    }))
     if (!map || !mapDesc || roomId === undefined) return;
     this.forEachClient((client) => {
       client.send(socketAction.sendRoom({
         map,
         mapDesc,
         roomId,
+        playerData,
       }));
     })
   }
@@ -121,7 +169,7 @@ class GameRoom {
 
   onTimeout() {
     this.stopTimer();
-    this.endGame();
+    // this.endGame();
   }
 
   onTimerInterval() {
@@ -180,6 +228,13 @@ class GameRoom {
     }
   }
 
+  resetTimers() {
+    this.stopTimer();
+    this.stopPrepareTimer();
+    this.leftTime = this.gameDuration;
+    this.leftPrepareTime = 3;
+  }
+
   restartTimer() {
     this.startTimer(this.leftTime);
   }
@@ -195,7 +250,7 @@ class GameRoom {
   checkWinner(winner?: number) {
     if (winner) {
       this.winner = winner;
-    } else {
+    } else if (this.mapDesc) {
       let requiredScore = this.mapDesc.maxScore;
       this.forEachPlayer((player) => {
         if (!this.winner && player.score === requiredScore) {
@@ -225,7 +280,7 @@ class GameRoom {
         const newData = new MultiPlay;
         newData.user1 = this.players[0].id;
         newData.user2 = this.players[1].id;
-        newData.difficulty = this.mapDesc.difficulty;
+        newData.difficulty = this.mapDesc?.difficulty || -1;
         newData.timeConsumed = this.gameDuration - this.leftTime;
         newData.winner = this.winner;
         multiRepo.save(newData)
@@ -241,6 +296,45 @@ class GameRoom {
   informWinner() {
     this.forEachClient((client) => {
       client.send(socketAction.informWinner(this.winner))
+    })
+    this.saveResult();
+  }
+
+  askRematch(userId: number) {
+    this.forEachPlayer((player) => {
+      if (player.id === userId) {
+        player.client.send(socketAction.askRematch())
+      }
+    })
+  }
+
+  cancelRematchAsk(userId: number) {
+    this.forEachPlayer((player) => {
+      if (player.id === userId) {
+        player.client.send(socketAction.cancelRematchAsk())
+      }
+    })
+  }
+
+  alertRematchDeclined(userId: number) {
+    this.forEachPlayer((player) => {
+      if (player.id === userId) {
+        player.client.send(socketAction.alertRematchDeclined())
+      }
+    })
+  }
+
+  informRematchAccepted(userId: number) {
+    this.forEachPlayer((player) => {
+      if (player.id === userId) {
+        player.client.send(socketAction.informRematchAccepted());
+      }
+    })
+  }
+
+  informPrepareRematch() {
+    this.forEachClient((client) => {
+      client.send(socketAction.informPrepareRematch());
     })
   }
 
