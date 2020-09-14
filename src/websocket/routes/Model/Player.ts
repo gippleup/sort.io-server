7e18a6b41c7bb39dbe471940ac4c243e6c7caa36
monthem <lpoeh01@gmail.com>
2020-09-14 20:26:56 +0900

    MODIFY: added prop to Player: player.name, player.receivedMap

--- src/websocket/routes/Model/Player.ts
+++ src/websocket/routes/Model/Player.ts
@@ -3,19 +3,23 @@ import Ws from 'ws';
 type PlayerConstructor = {
   ws: Ws,
   id: number,
+  name: string,
 }
 
 class Player {
   client: Ws;
   id: number;
+  name: string;
   score: number = 0;
   isReady: boolean = false;
   isPrepared: boolean = false;
+  receivedMap: boolean = false;
   hasLeftGame: boolean = false;
   constructor(option: PlayerConstructor) {
-    const {ws, id} = option;
+    const {ws, id, name} = option;
     this.client = ws;
     this.id = id;
+    this.name = name;
   }
 }
 
