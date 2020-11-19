197ceb431d6d9928592825cd0dc69dd7f10a1d13
monthem <lpoeh01@gmail.com>
2020-11-19 19:32:34 +0900

    FIX: hot fix for multigame

--- src/websocket/routes/utils/waitingLine.ts
+++ src/websocket/routes/utils/waitingLine.ts
@@ -1,9 +1,9 @@
 import { getMultiPlayRankByUserId } from "../../../utils/multiPlay";
 
 export const getLineIndex = (userId: number): Promise<null | number> => {
-  return getMultiPlayRankByUserId(userId)
+  return getMultiPlayRankByUserId(userId, 0, 3650000)
     .then((userRankData) => {
-      if (!userRankData) return null;
+      if (!userRankData) return 0;
       const KBI = userRankData?.targetUser.KBI;
       const lineIndex = Math.floor(KBI / 5);
       return lineIndex;
